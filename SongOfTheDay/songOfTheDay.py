dfc5d190611ec327d70b2def12d0f6a61d754460
Tomasz <dworowytomasz@gmail.com>
2016-12-20 17:43:31 +0100

    update

--- SongOfTheDay/songOfTheDay.py
+++ SongOfTheDay/songOfTheDay.py
@@ -1,26 +1,30 @@
 import unittest
+
+import time
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+class songOfTheDay():
+
+    def __init__(self):
+        self.setUp()
+
+    def sendMessageToSelected(self,message):
+        element = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.TAG_NAME, 'swx-textarea')))
+        element = element.find_element_by_tag_name("textarea")
+        element.click()
+        element.clear()
+        element.send_keys(message)
+        element.send_keys(Keys.ENTER)
 
 
 
-class songOfTheDay(unittest.TestCase):
-
-    def findSong(self, song):
-        self.driver.get('https://www.youtube.com')
-        input = self.driver.find_element_by_id("masthead-search-term")
-        searchButton = self.driver.find_element_by_css_selector("button[type='submit']")
-        input.click()
-        input.send_keys(song)
-        searchButton.click()
-
-        self.driver.implicitly_wait(5)
-        firstResoult = self.driver.find_element_by_css_selector("h3")
-        firstResoult.click()
-        self.driver.implicitly_wait(3)
-        return self.driver.current_url
-
-    def sentSong(self,autentycation,songURL):
+    def  login(self,autentycation ):
         self.driver.get('https://web.skype.com/pl/')
         self.driver.implicitly_wait(3)
         loginFB = self.driver.find_element_by_id("loginWithFacebook")
@@ -36,22 +40,49 @@ class songOfTheDay(unittest.TestCase):
 
         loginButton = self.driver.find_element_by_id("loginbutton")
         loginButton.click()
-        self.driver.implicitly_wait(60)
+        WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.CLASS_NAME, 'summary')))
+        WebDriverWait(self.driver, 30).until_not(EC.visibility_of_element_located((By.CLASS_NAME, 'shellSplashContent')))
 
+    def select(self,name):
         searchSkype = self.driver.find_element_by_css_selector("div.input input.inputField.fontSize-h4")
         searchSkype.click()
+        time.sleep(1)
+
         searchSkype = self.driver.find_element_by_css_selector("div.input.active input.inputField.fontSize-h4")
-        searchSkype.send_keys("A smiechom i szopom nie było konca")
+        searchSkype.send_keys(name)
         self.driver.implicitly_wait(5)
         group = self.driver.find_element_by_class_name("list-selectable")
         group.click()
         self.driver.implicitly_wait(5)
-        messageImput = self.driver.find_element_by_css_selector("div#chatInputContainer.container textarea#chatInputAreaWithQuotes")# fail
-        messageImput.click()
-        messageImput.send_keys("Piosenka Dnia [AUTO]")
-        messageImput.send_keys(Keys.ENTER)
-        messageImput.send_keys(songURL)
-        messageImput.send_keys(Keys.ENTER)
+        time.sleep(2)
+
+
+
+    def findSong(self, song):
+        self.driver.get('https://www.youtube.com')
+        input = self.driver.find_element_by_id("masthead-search-term")
+        searchButton = self.driver.find_element_by_css_selector("button[type='submit']")
+        input.click()
+        input.send_keys(song)
+        searchButton.click()
+
+        self.driver.implicitly_wait(5)
+        firstResoult = self.driver.find_element_by_css_selector("h3")
+        firstResoult.click()
+        self.driver.implicitly_wait(3)
+        return self.driver.current_url
+
+
+
+
+
+    def sentSong(self,autentycation,songURL):
+        self.login(autentycation)
+
+        self.select("A smiechom i szopom nie było konca")
+
+        self.sendMessageToSelected("Piosenka dina [Auto]")
+        self.sendMessageToSelected("songURL")
 
     def setUp(self):
         self.driver = webdriver.Chrome()
@@ -62,8 +93,27 @@ class songOfTheDay(unittest.TestCase):
         self.driver.quit()
 
     def testsongOfTheDay(self):
-         self.sentSong(["**","***"],self.findSong("paranoid")) # TODO load data from file
+         self.sentSong(["dworowytomasz@gmail.com","Jefferson Airplane1966!"],self.findSong("paranoid")) # TODO load data from file
+
+
+
+
+def main():
+  try:
+
+      song = songOfTheDay()
+
+      autentycation = ["dworowytomasz@gmail.com","Jefferson Airplane1966!"]
+
+      url= song.findSong("paranoid")
+      song.sentSong(autentycation , url)
+      song.tearDown()
+
+  except Exception  as err:
+    print(err)
+  finally:
+    quit()
 
 
 if __name__ == '__main__':
-    unittest.main()
+    main()
