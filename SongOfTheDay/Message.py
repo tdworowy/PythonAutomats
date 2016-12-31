1ea6ee19daa5e223d97c0909698460ae8e97cb94
Tomasz <dworowytomasz@gmail.com>
2016-12-31 21:30:47 +0100

    update

---
+++ SongOfTheDay/Message.py
@@ -0,0 +1,28 @@
+import os
+
+from SongOfTheDay.songOfTheDay import songOfTheDay
+
+
+def main(login, password):
+    try:
+        autentycation = [login,password]
+        message = songOfTheDay()
+        message.login(autentycation)
+
+        message.select("Echo")
+        message.select("A smiechom i szopom nie było konca")
+        message.sendMessageToSelected("Szczęsliwego Nowego roku !! [Auto]")
+    except Exception  as err:
+        print(err)
+
+    finally:
+        quit()
+
+
+
+if __name__ == '__main__':
+    # var1 = input("Login: ")
+    #var2 = input("Pass: ")
+   f= open(os.path.dirname(os.path.abspath(__file__))+'\\aut.txt')
+   main(f.readline().strip(), f.readline().strip())
+   main()
