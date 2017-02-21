da0e064fc5d05cc60d238e69ca2dd8718efb0ff6
tdworowy <dworowytomasz@gmail.com>
2017-02-21 14:39:26 +0100

    Update utils.py
    fix

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import time
 
 def log (text):
     timeStumo = time.strftime('%Y-%m-%d %H:%M:%S')
-    log = timeStumo+" "+text +"/n"
+    log = timeStumo+" "+text +"\n"
     print(log)
     with open("log.txt", "a+") as logFile:
         logFile.write(log)
