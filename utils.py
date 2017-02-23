a4f00d7e50e2b518611c05a15a3b996fdd86644b
Tomasz <dworowytomasz@gmail.com>
2017-02-23 18:34:45 +0100

    update

--- utils.py
+++ utils.py
@@ -1,8 +1,10 @@
+import os
 import time
 
 def log (text):
     timeStumo = time.strftime('%Y-%m-%d %H:%M:%S')
     log = timeStumo+" "+text +"\n"
     print(log)
-    with open("log.txt", "a+") as logFile:
+    path = os.path.dirname(os.path.abspath(__file__))+"\\log.txt"
+    with open( path, "a+") as logFile:
         logFile.write(log)
