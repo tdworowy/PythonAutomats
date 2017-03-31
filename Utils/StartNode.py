abef8bc94d9bb89bfa308853cef4c00c3e57196b
thomas <dworowytomasz@gmail.com>
2017-03-31 17:54:33 +0200

    update for selenium grid

--- Utils/StartNode.py
+++ Utils/StartNode.py
@@ -2,7 +2,7 @@ from chromedriverFolder.driverPath import getDriverPath
 
 seleniumServerPath = "D:\selenium"
 driverPath =getDriverPath()+ '/chromedriver.exe'
-node = "java -jar -Dwebdriver.chrome.driver="+driverPath+"selenium-server-standalone-3.3.1.jar -host localhost -role webdriver -hub http://192.168.0.102:4444/grid/register  -port 5555 -browser browserName=chrome,maxInstances=5,platform=WINDOWS"
+node = "java -jar -Dwebdriver.chrome.driver="+driverPath+" selenium-server-standalone-3.3.1.jar -host localhost -role webdriver -hub http://192.168.0.102:4444/grid/register  -port 5555 -browser browserName=chrome,maxInstances=5,platform=WINDOWS"
 
 
 nodeExecution = "cd "+ seleniumServerPath+ "&& D: &&"+node
---
+++ Utils/startNodeLinuks.py
@@ -0,0 +1,17 @@
+from chromedriverFolder.driverPath import getDriverPath
+
+seleniumServerPath = "/home/thomas/selenium"
+driverPath =getDriverPath()+ '/chromedriver.exe'
+node = "java -jar -Dwebdriver.chrome.driver="+driverPath+" selenium-server-standalone-3.3.1.jar -host localhost -role webdriver -hub http://192.168.0.102:4444/grid/register  -port 5555 -browser browserName=chrome,maxInstances=5,platform=LINUX"
+
+
+nodeExecution = "cd "+ seleniumServerPath+ " &&"+node
+
+print(nodeExecution)
+
+
+def run(comand):
+    import os
+    os.system(comand)
+
+run(nodeExecution)
