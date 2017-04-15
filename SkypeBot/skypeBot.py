7d1b90e3cbb473bb56787f31765cb1ee078c213c
Tomasz <dworowytomasz@gmail.com>
2017-04-15 23:01:33 +0200

    Refactor

--- SkypeBot/skypeBot.py
+++ SkypeBot/skypeBot.py
@@ -6,9 +6,26 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
+
 class skypeBot():
+    def initializeElements(self):
+        self.loginFB = (By.ID, 'loginWithFacebook')
+        self.loginEmail = (By.ID, 'email')
+        self.loginPassword = (By.ID, 'pass')
+        self.loginButton = (By.ID, 'pass')
+        self.inputField = (By.CSS_SELECTOR, "div.input input.inputField.fontSize-h4")
+
+        self.loginSkype = (By.ID, 'username')
+        self.loginSkypeButton = (By.ID, 'signIn')
+        self.loginSkypePassword = (By.NAME, 'passwd')
+
+        self.group = (By.CLASS_NAME, 'list-selectable')
+
+
+
     def __init__(self,webdriver):
         self.driver= webdriver
+        self.initializeElements()
 
 
 
@@ -25,53 +42,54 @@ class skypeBot():
         actions.perform()
 
 
-    def loginFacebook(self, autentycation):
+    def openSkype(self):
         self.driver.get('https://web.skype.com/pl/')
-        self.driver.implicitly_wait(3)
-        loginFB = self.driver.find_element_by_id("loginWithFacebook")
+
+    def waitForInputField(self):
+        WebDriverWait(self.driver, 40).until(EC.visibility_of_any_elements_located(self.inputField))
+        time.sleep(1)
+
+    def loginFacebook(self, autentycation):
+        self.openSkype()
+
+        loginFB = self.driver.find_element(*self.loginFB)
         loginFB.click()
 
-        loginField = self.driver.find_element_by_id("email")
+        loginField = self.driver.find_element(*self.loginEmail)
         loginField.click()
         loginField.send_keys(autentycation[0])
 
-        passField = self.driver.find_element_by_id("pass")
+        passField = self.driver.find_element(*self.loginPassword)
         passField.click()
         passField.send_keys(autentycation[1])
 
-        loginButton = self.driver.find_element_by_id("loginbutton")
+        loginButton = self.driver.find_element(*self.loginButton)
         loginButton.click()
-        WebDriverWait(self.driver, 40).until(EC.visibility_of_any_elements_located((By.CSS_SELECTOR, "div.input input.inputField.fontSize-h4")))
-        time.sleep(1)
+        self.waitForInputField()
 
     def login(self, autentycation):
-        self.driver.get('https://web.skype.com/pl/')
-
-        loginField = self.driver.find_element_by_id("username")
+        self.openSkype()
+        loginField = self.driver.find_element(*self.loginSkype)
         loginField.click()
         loginField.send_keys(autentycation[0])
 
-        loginButton = self.driver.find_element_by_id("signIn")
+        loginButton = self.driver.find_element(*self.loginSkypeButton)
         loginButton.click()
 
-        passField = self.driver.find_element_by_name("passwd")
+        passField = self.driver.find_element(*self.loginSkypePassword)
         passField.click()
         passField.send_keys(autentycation[1])
 
         self.sendEnter()
-
-        WebDriverWait(self.driver, 40).until(EC.visibility_of_any_elements_located((By.CSS_SELECTOR, "div.input input.inputField.fontSize-h4")))
-        time.sleep(1)
-
+        self.waitForInputField()
 
     def select(self, name):
-        searchSkype = self.driver.find_element_by_css_selector("div.input input.inputField.fontSize-h4")
+        searchSkype = self.driver.find_element(*self.inputField)
         searchSkype.click()
         time.sleep(1)
 
-        searchSkype = self.driver.find_element_by_css_selector("div.input.active input.inputField.fontSize-h4")
         searchSkype.send_keys(name)
         time.sleep(1)
-        group = self.driver.find_element_by_class_name("list-selectable")
+        group = self.driver.find_element(*self.group)
         group.click()
         time.sleep(2)
