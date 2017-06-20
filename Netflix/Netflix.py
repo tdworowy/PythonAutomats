c634d6c95fe21a82926fe106447f4774c8b1b9e3
Tomasz <dworowytomasz@gmail.com>
2017-06-20 19:01:46 +0200

    update

--- Netflix/Netflix.py
+++ Netflix/Netflix.py
@@ -28,13 +28,13 @@ def getAllCategories(login,password):
     actions.send_keys(Keys.ENTER).perform()
 
     driver.find_element(*avatar).click()
-
+    f = open("cat", 'w')
     for i in range(1,9999):
 
         driver.get(generUrl+str(i))
         try:
             category = driver.find_element(*genreTitle).text
-            print((category,i))
+            print((category,i),file=f)
 
         except:
             continue
