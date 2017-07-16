3bec5c8a8ec23216d05dbbac79c8e8dae409fd26
Tomasz <dworowytomasz@gmail.com>
2017-07-16 15:41:39 +0200

    update

--- Utils/Songs_/Songs.py
+++ Utils/Songs_/Songs.py
@@ -20,21 +20,15 @@ PAGES = 706
 def getFilePath():
     return filePath
 
-def getTitels(count,url):
-    log("Get songs from last fm --- START")
-    clrTitels = []
-    for i in range(1,count):
-        log("Get songs from page %s" % i)
-        response = requests.get(url + str(i)).text
-        soup = BeautifulSoup(response,"html.parser")
-        titles = soup.find_all("a", class_="link-block-target")
-        titles = str(titles)
-        titles = titles.split(">")
-        newTitles = [title.encode("utf-32").decode('utf-32','ignore') for title in titles] # ??
-        # print(newTitles)
-        clrTitels.extend(newTitles)
-        log("Get songs from page %s --- DONE" % i)
-    return clrTitels
+def getTitles(url):
+    log("Get songs from %s" % url )
+    response = requests.get(url).text
+    soup = BeautifulSoup(response,"html.parser")
+    titles = soup.find_all("a", class_="link-block-target")
+    titles = str(titles)
+    titles = titles.split(">")
+    newTitles = clearTitels([title.encode("utf-32").decode('utf-32','ignore') for title in titles])
+    return newTitles
 
 def clearTitels(titles):
     cleanTitels = []
@@ -57,21 +51,25 @@ def getSongs():
             log("Generate songs list")
             log("Clear existing or create new file")
             open(filePath, 'w').close()
-            count = 0
-            titles =clearTitels(getTitels(PAGES,'https://www.last.fm/pl/user/TotaledThomas/library/tracks?page='))
-            print("Songs found: %s" % str(len(titles)))
-                #print(titles)
-            with open(filePath, 'a') as f:
-                for text in titles:
-                    try:
-                         count +=1
-                         f.write(text)
-                         f.flush()
-                    except Exception as ex:
-                         log("EXEPTION while generating songs list")
-                         log(str(ex))
-                         continue
-                log("Songs count: %s" % str(count))
+            for i in range(1,PAGES):
+                titles =getTitles('https://www.last.fm/pl/user/TotaledThomas/library/tracks?page= %s' % str(i))
+                toFile(titles)
+
+
+
+def toFile(titels):
+    count = 0
+    with open(filePath, 'a') as f:
+        for text in titels:
+            try:
+                count += 1
+                f.write(text)
+                f.flush()
+            except Exception as ex:
+                log("EXEPTION while generating songs list")
+                log(str(ex))
+                continue
+        log("Songs count: %s" % str(count))
 
 
 
@@ -87,17 +85,18 @@ def updateSongs():
     log("Files opened Correctly")
     oldTitels = [line for line in f1.readlines()]
     # newTitles = clearTitels(getTitels(10,"http://www.last.fm/pl/user/TotaledThomas/library?date_preset=LAST_7_DAYS&page="))
-    newTitles = clearTitels( getTitels(60, "https://www.last.fm/pl/user/TotaledThomas/library?date_preset=LAST_30_DAYSS&page="))
-    log("New titles: "+str(newTitles))
-    for title in newTitles:
-          try:
-              if title not in oldTitels:
-                  f2.write(title)
-                  f2.flush()
-          except Exception as ex:
-                  log("Error while updating songs list")
-                  log(str(ex))
-                  continue
+    for i in range(1,60):
+        newTitles = getTitles("https://www.last.fm/pl/user/TotaledThomas/library?date_preset=LAST_30_DAYSS&page=%s" % i)
+        log("New titles: "+str(newTitles))
+        for title in newTitles:
+              try:
+                  if title not in oldTitels:
+                      f2.write(title)
+                      f2.flush()
+              except Exception as ex:
+                      log("Error while updating songs list")
+                      log(str(ex))
+                      continue
     f2.flush()
     f2.close()
     log("Song List updated correctly")
