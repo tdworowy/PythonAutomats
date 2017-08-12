383403d96503442e9b372af688728cb03b837cb1
Tomasz <dworowytomasz@gmail.com>
2017-08-12 15:27:18 +0200

    update

---
+++ GoogleDocks/googleApi.py
@@ -0,0 +1,63 @@
+
+import os
+
+import httplib2
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+#TODO
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+CLIENT_SECRET_FILE = 'D:\Google_drive\client_secret_332972022991-tvog4ehs3basgujc43hnqdlkg9ph1gqd.apps.googleusercontent.com.json'
+APPLICATION_NAME = 'Google Sheets API'
+
+
+def get_credentials():
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'sheets.googleapis.com-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        credentials = tools.run_flow(flow, store, flags)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def readDocument():
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
+                    'version=v4')
+    service = discovery.build('sheets', 'v4', http=http,
+                              discoveryServiceUrl=discoveryUrl)
+
+    spreadsheetId = '1XPWKt_ZjtytJQ48yzIeH5WjAPfrQ4Yuw6T0x8FG62Jw'
+    # result = service.spreadsheets().values().get(
+    #     spreadsheetId=spreadsheetId, range=rangeName).execute()
+    result = service.
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+    else:
+        print('Name, Major:')
+        for row in values:
+            # Print columns A and E, which correspond to indices 0 and 4.
+            print('%s, %s' % (row[0], row[4]))
+
+
+if __name__ == '__main__':
+    main()
