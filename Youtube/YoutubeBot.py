2c7bc6d7812a2077945fcb69c7eb425ddded92c2
Tomasz <dworowytomasz@gmail.com>
2017-09-01 18:56:27 +0200

    fix

--- Youtube/YoutubeBot.py
+++ Youtube/YoutubeBot.py
@@ -4,15 +4,17 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
 
 
-def getYoutubeURL(driver, phraze):
-    driver.get('https://www.youtube.com')
+def actionSend(driver,txt):
     actions = ActionChains(driver)
-
-    input = driver.find_element_by_id("search")
-    input.click()
-    input.send_keys(phraze)
+    actions.send_keys(txt)
     actions.send_keys(Keys.ENTER)
     actions.perform()
+    time.sleep(1)
+
+
+def getYoutubeURL(driver, phraze):
+    driver.get('https://www.youtube.com')
+    actionSend(driver,phraze)
     time.sleep(2)
     firstResoult = driver.find_element_by_css_selector("h3 a")
 
