cc6ec7cb4a11b4551ef9982883a9ae0ae1f66420
Tomasz <dworowytomasz@gmail.com>
2017-09-16 19:10:02 +0200

    update

--- Calculator/Behave/run.py
+++ Calculator/Behave/run.py
@@ -1,10 +1,14 @@
 import os
 
+from Calculator.Behave.screens.screenPath import getScreenPath
+
+
 def runBehave():
     path = os.path.dirname(os.path.abspath(__file__))
     partition = "D:"
     # commend = partition + " && cd " + path + " && behave"
-    commend = partition+" && cd "+path + " && behave -i (calculator2)"
+    commend = '%s && cd "%s" && behave -f allure_behave.formatter:AllureFormatter -o "%s" ./features' % (
+    partition, path, getScreenPath())
     print(commend)
     os.system(commend)
 
