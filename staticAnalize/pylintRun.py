c3bdb9bd6877802e0252e34c48e0ad3d4b125dc6
Tomasz <dworowytomasz@gmail.com>
2017-09-19 21:35:46 +0200

    update

--- staticAnalize/pylintRun.py
+++ staticAnalize/pylintRun.py
@@ -6,7 +6,7 @@ from projectPath import getProjectPath
 
 
 def runpylint(module):
-     os.system("cd %s && pylint --output-format=json %s >> report%s.json" % (getProjectPath(),getMilis(),module))
+     os.system("cd %s && pylint --output-format=json %s >> report%s.json" % (getProjectPath(),module,getMilis()))
 
 if __name__ == '__main__':
     runpylint(sys.argv[1])
