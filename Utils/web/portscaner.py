8e1c896e10298885f48877d9a1d34cb4fc3143e2
Tomasz <dworowytomasz@gmail.com>
2017-10-21 14:55:21 +0200

    update

--- Utils/web/portscaner.py
+++ Utils/web/portscaner.py
@@ -1,32 +1,31 @@
-import sys
 from socket import *
 
 from Utils.decorators import log_exception
 from Utils.utils import log
 
 
-def scan_host(host, port, debug=False):
-    s = socket(AF_INET, SOCK_STREAM)
-    code = s.connect_ex((host, port))
-    s.close()
-    if debug: print("Port checked: %s response %s" % (port, code))
-    return port, code
+class PortScanner:
+    def __init__(self, host):
+        self.soc = socket(AF_INET, SOCK_STREAM)
+        self.host = host
 
+    def scan_host(self, port, debug=False):
+        code = self.soc.connect_ex((self.host, port))
+        self.soc.close()
+        if debug: print("Port checked: %s response %s" % (port, code))
+        return port, code
 
-def _scan_host(hp):
-    return scan_host(hp[0], hp[1])
-
-
-@log_exception()
-def scan_ports(host, min, max):
-    host_ip = gethostbyname(host)
-    print("Host: %s IP: %s" % (host, host_ip))
-    print("Scan in progress...")
-    results = list(map(_scan_host, [(host, port) for port in range(min, max + 1)]))
-    print("Scan Done...")
-    return [x[0] for x in results if x[1] == 0]
+    @log_exception()
+    def scan_ports(self, min, max):
+        host_ip = gethostbyname(self.host)
+        print("Host: %s IP: %s" % (self.host, host_ip))
+        print("Scan in progress...")
+        results = map(self.scan_host, [port for port in range(min, max + 1)])
+        print("Scan Done...")
+        return [x[0] for x in results if x[1] == 0]
 
 
 if __name__ == '__main__':
-    open_ports = scan_ports(sys.argv[1], 0, 65535)
+    ps = PortScanner(sys.argv[1])
+    open_ports = ps.scan_ports(0, 65535)
     log("Open Ports:", open_ports)
