bb8b187c89960a502a39065aba66a5804c39ff38
Tomasz <dworowytomasz@gmail.com>
2017-10-22 09:49:34 +0200

    update

--- Utils/web/portscaner.py
+++ Utils/web/portscaner.py
@@ -13,37 +13,50 @@ class PortScanner:
         self.path = os.path.dirname(os.path.abspath(__file__))
 
     def scan_host(self, port, debug=False):
-        soc = socket(AF_INET, SOCK_STREAM)
-        code = soc.connect_ex((self.host, port))
-        soc.close()
+        code = self.soc.connect_ex((self.host, port))
         if debug: print("Port checked: %s response %s" % (port, code))
         if code == 0: print("Port %s is open" % port)
         return port, code
 
     @log_exception()
     def scan_ports(self, min, max):
-
+        self.soc = socket(AF_INET, SOCK_STREAM)
         with open("%s\\results\Ports_%s.txt" % (self.path, max), "w") as f1:
             host_ip = gethostbyname(self.host)
             print("Host: %s IP: %s" % (self.host, host_ip))
             print("Scan in progress...")
             results = map(self.scan_host, [port for port in range(min, max + 1)])
+            self.soc.close()
             opened_ports = [x[0] for x in list(results) if x[1] == 0]
             print("Scan Done...")
             f1.write(str(opened_ports).replace("[", "").replace("]", ""))
 
 
-if __name__ == '__main__':
-    host_ = sys.argv[1]
-    ps = PortScanner(host_)
-    try:
-        _thread.start_new_thread(ps.scan_ports, (0, 16384))
-        _thread.start_new_thread(ps.scan_ports, (16384, 49150))
-        _thread.start_new_thread(ps.scan_ports, (49150, 55534))
-        _thread.start_new_thread(ps.scan_ports, (55534, 65534))
-    except Exception as e:
-        print("thread error: %s" % str(e))
+@log_exception()
+def distribution(ps, number, parts):
+    rest = number % parts
+    min = 0
+    max = number / parts
+    for i in range(1, parts):
+        if i == parts: max = max + rest
+        max = i * max
+        _thread.start_new_thread(ps.scan_ports, (min, max))
+        min = max
 
+
+def main(host="127.0.0.1", min=0, max=65534):
+    ps = PortScanner(host)
+    distribution(ps,min, max)
     time.sleep(120)
-    while _thread._count() >0:
+    while _thread._count() > 0:
         time.sleep(120)
+
+
+if __name__ == '__main__':
+    if sys.argv> 1:
+        host_ = sys.argv[1]
+        min = sys.argv[2]
+        max = sys.argv[3]
+        main(host=host_,min=min,max=max)
+    else:
+        main()
