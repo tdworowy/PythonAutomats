c8e439d54fe5f240282f4555671ef85fc6958c68
Tomasz <dworowytomasz@gmail.com>
2017-11-25 10:48:34 +0100

    fix

--- Utils/Songs_/Songs.py
+++ Utils/Songs_/Songs.py
@@ -22,7 +22,7 @@ def get_pages_count(url):
     soup = BeautifulSoup(response, "html.parser")
     pagination_list = soup.find('ul', class_="pagination-list")
     pages = pagination_list.find_all('a')
-    return max([page.text for page in pages[:-1]])
+    return int(max([page.text for page in pages[:-1]]))
 
 
 def get_titles(url):
@@ -58,7 +58,7 @@ def get_songs(user='TotaledThomas'):
     open(FILE_PATH, 'w').close()
     url = 'https://www.last.fm/pl/user/%s/library/tracks' % user
     pages_count = get_pages_count(url)
-    titles = map(get_titles,[url+'?page= %s' % str(i) for i in range(1, pages_count+1)])
+    titles = map(get_titles, [url + '?page= %s' % str(i) for i in range(1, pages_count + 1)])
     to_file(titles)
 
 
@@ -87,21 +87,20 @@ def update_songs(user='TotaledThomas', pages_to_check=60):
     with open(FILE_PATH) as f1:
         old_titles = [line for line in f1.readlines()]
     url = "https://www.last.fm/pl/user/%s/library?date_preset=LAST_30_DAYSS" % user
-    new_titles = map(get_titles,[url+"&page=%s" % str(i) for i in range(1, pages_to_check+1)])
+    new_titles = map(get_titles, [url + "&page=%s" % str(i) for i in range(1, pages_to_check + 1)])
     titles_to_update = [title for title in new_titles if title not in old_titles]
     with open(FILE_PATH, 'a') as f2:
         for title in titles_to_update:
-                try:
-                    f2.write(title)
-                    f2.flush()
-                except Exception as ex:
-                    log("Error while updating songs list")
-                    log(str(ex))
-                    continue
+            try:
+                f2.write(title)
+                f2.flush()
+            except Exception as ex:
+                log("Error while updating songs list")
+                log(str(ex))
+                continue
     log("Song List updated correctly")
     open(LAST_UPDATED, 'w').write(str(date_today))
 
 
 if __name__ == '__main__':
-   get_songs()
-
+    get_songs()
