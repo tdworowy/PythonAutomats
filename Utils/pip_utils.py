bf61880cdabe9cf06a02b9724dec694718a21543
Tomasz <dworowytomasz@gmail.com>
2018-01-20 23:52:48 +0100

    update

--- Utils/pip_utils.py
+++ Utils/pip_utils.py
@@ -1,3 +1,5 @@
+from subprocess import call
+
 import pip
 
 
@@ -5,8 +7,19 @@ def generate_reg_txt():
     packages = [dist.project_name for dist in pip.get_installed_distributions()]
     with open("req.txt", 'w') as f:
         for package in packages:
-            f.write(package+"\n")
+            f.write(package + "\n")
+
+
+def install_all(file):
+    with open(file)as f:
+        for line in f.readlines():
+            try:
+                call("pip install --upgrade " + str(line), shell=True)
+            except Exception as ex:
+                print(ex)
+                continue
 
 
 if __name__ == "__main__":
-    generate_reg_txt()
+    # generate_reg_txt()
+    install_all("req.txt")
