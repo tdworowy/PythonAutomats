bbd240d89622635ede6ff803746fb3d49d512e14
Tomasz <dworowytomasz@gmail.com>
2018-02-09 10:07:51 +0100

    update song on demand face

--- Facebook/facebook_monitor.py
+++ Facebook/facebook_monitor.py
@@ -1,54 +1,46 @@
-import sys
 import time
+from threading import Thread
 
 from Facebook.facebook_apiI import FaceBookMessageBot
 from Utils.utils import create_file_if_not_exist
 
 
 class FaceThreadMonitor:
-    def __init__(self, email, passw, path):
+    def __init__(self, email, passw, path, thread_ID):
         self.face_bot = FaceBookMessageBot()
         self.face_bot.login(email, passw)
         self.path = path
+        self.thread_ID = thread_ID
 
-    def monitor_threads(self, phrase, threads_ids):
-        for thread in threads_ids:
-            for massage in self.face_bot.get_messages(thread):
-                if str(massage.text) == phrase:
-                    path = self.path + thread + ".txt"
-                    create_file_if_not_exist(path)
-                    with open(path, 'r+') as f:
-                        msg = str((massage.text, massage.timestamp))
-                        line_found = any(msg in line for line in f)
-                        if not line_found:
-                            f.write(msg + "\n")
-                            f.flush()
+    def monitor_thread(self, phrase):
+        for massage in self.face_bot.get_messages(self.thread_ID):
+            if str(massage.text) == phrase:
+                path = self.path + self.thread_ID + ".txt"
+                create_file_if_not_exist(path)
+                with open(path, 'r+') as f:
+                    msg = str((massage.text, massage.timestamp))
+                    line_found = any(msg in line for line in f)
+                    if not line_found:
+                        f.write(msg + "\n")
+                        f.flush()
 
-    def set_thread_ides(self, threadIDs):
-        self.threadIDs = threadIDs
-
-
-def start_monitor(phraze, face_thread_monitor_list):
-    while 1:
-        for ftm in face_thread_monitor_list:
-            ftm.monitor_threads(phraze, ftm.threadIDs)
+    def monitor(self, phrase):
+        while 1:
+            self.monitor_thread(phrase)
             time.sleep(60)
 
 
-if __name__ == '__main__':
-    # THREADIDs = ['1252344071467839','100000471818643']
-    THREADIDs1 = ['1252344071467839']  # group
-    THREADIDs2 = ['100000471818643']  # user
+def start_monitor(phraze, face_thread_monitor_list):
+        threads = []
+        for ftm in face_thread_monitor_list:
+            try:
+                thread = Thread(target=ftm.monitor, args=(phraze,))
+                threads.append(thread)
+                thread.start()
+            except Exception:
+                import traceback
+                traceback.print_exc()
 
-    user = sys.argv[1]
-    passw = sys.argv[2] + " " + sys.argv[3]
-    path1 = 'D:\Google_drive\QueesGroup\\'
-    path2 = 'D:\Google_drive\QueesUser\\'
+        for thread in threads:
+            thread.join()
 
-    fm1 = FaceThreadMonitor(user, passw, path1)
-    fm1.set_thread_ides(THREADIDs1)
-
-    fm2 = FaceThreadMonitor(user, passw, path2)
-    fm2.set_thread_ides(THREADIDs2)
-
-    start_monitor("[SONG]", [fm1, fm2])
--- Facebook/song_on_demand_face.py
+++ Facebook/song_on_demand_face.py
@@ -2,11 +2,15 @@ import os
 import random
 import sys
 import time
+from functools import partial
+from multiprocessing import Process
 from os import listdir
 from os.path import isfile, join
+from threading import Thread
 
 from fbchat import ThreadType
 
+from Facebook.facebook_monitor import FaceThreadMonitor, start_monitor
 from Facebook.song_of_the_day_facebook_message import SongOfTheDayFace
 from Utils.Songs_.Songs import FILE_PATH
 from Utils.file_utils import create_file_if_not_exist
@@ -14,25 +18,28 @@ from Utils.utils import log, save_history
 from Youtube.Youtube_Bot import get_youtube_URL
 
 
-def check_queue(path):
-    files = [f for f in listdir(path) if isfile(join(path, f))]
+def get_ides(path, file, checked):
     ids = []
-    for file in files:
-        file_name = os.path.splitext(file)[0]
-        checked = path + "checked\\" + file_name + "_checked.txt"
-        create_file_if_not_exist(checked)
-        f2 = open(checked, 'r+')
-        with open(path + "\\" + file, 'r') as f:
-            for line in f.readlines():
-                line_found = any(line in line2 for line2 in f2)
-                if not line_found:
-                    print(file_name)
-                    ids.append(file_name)
-                    f2.write(line + '\n')
+    file_name = os.path.splitext(file)[0]
+    f2 = open(checked, 'r+')
+    with open(path + "\\" + file, 'r') as f:
+        for line in f.readlines():
+            if not any(line in line2 for line2 in f2):
+                print(file_name)
+                ids.append(file_name)
+                f2.write(line + '\n')
     return ids
 
 
-def main(song_, thread_id, thread_type):
+def check_queue(path):
+    files = [f for f in listdir(path) if isfile(join(path, f))]
+    checked_list = [path + "checked\\" + os.path.splitext(file)[0] + "_checked.txt" for file in files]
+    checked = map(create_file_if_not_exist, checked_list)
+    partial_get_ides = partial(get_ides, path)
+    return list(map(partial_get_ides, files, list(checked)))
+
+
+def send_song(song_, thread_id, thread_type):
     f = open(FILE_PATH, 'r')
     log("Get random song")
     songs = f.read()
@@ -45,25 +52,49 @@ def main(song_, thread_id, thread_type):
     url = get_youtube_URL(song_.driver, song_title.strip())
     song_.sent_song([url], thread_id, "SONG ON DEMAND", thread_type)
     song_.tear_down()
+    time.sleep(20)
 
 
-def thread(song_, path, thread_type):
-    threads = check_queue(path)
+def send_songs_threads(song_, path, thread_type):
+    threads_ids = check_queue(path)
+    threads = []
+    for thread_id in threads_ids:
+        try:
+            thread = Thread(target=send_song, args=(song_, thread_id, thread_type,))
+            threads.append(thread)
+            thread.start()
+        except Exception:
+            import traceback
+            traceback.print_exc()
+
     for thread in threads:
-        main(song_, thread, thread_type)
+        thread.join()
 
 
 if __name__ == '__main__':
     path1 = 'D:\Google_drive\QueueGroup\\'
     path2 = 'D:\Google_drive\QueueUser\\'
+    THREADID1 = '1252344071467839'  # group
+    THREADID2 = '100000471818643'  # user
 
     user = sys.argv[1]
     passw = sys.argv[2] + " " + sys.argv[3]
 
+    fm1 = FaceThreadMonitor(user, passw, path1, THREADID1)
+    fm2 = FaceThreadMonitor(user, passw, path2, THREADID2)
+
     song = SongOfTheDayFace()
     song.login_FB(user, passw)
 
+    process1 = Process(target=start_monitor, args=("[SONG]", [fm1, fm2]))
+    process2 = Process(target=send_songs_threads, args=(path1, ThreadType.GROUP))
+    process3 = Process(target=send_songs_threads, args=(path2, ThreadType.USER))
+
+    for process in [process1, process2, process3]:
+        process.start()
+
+    for process in [process1, process2, process3]:
+        process.join()
+
     while 1:
-        thread(song, path1, ThreadType.GROUP)
-        thread(song, path2, ThreadType.USER)
-        time.sleep(60)
+        pass
