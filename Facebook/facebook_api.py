c4989b75dc00fe7677e3feb64ae3b4e27618ee87
Tomasz <dworowytomasz@gmail.com>
2018-02-22 11:31:32 +0100

    update

--- Facebook/facebook_api.py
+++ Facebook/facebook_api.py
@@ -7,9 +7,11 @@ from fbchat.models import *
 
 class FaceBookMessageBot:
     def login(self, email, password):
+        self.email = email
+        self.passwd = password
         self.client = Client(email, password)
 
-    def send_message(self, message, thread_id, thread_type=ThreadType.GROUP,repeat_on_fail=7):
+    def send_message(self, message, thread_id, thread_type=ThreadType.GROUP, repeat_on_fail=7):
         # self.client.sendMessage(message, thread_id=thread_id, thread_type=thread_type)
         repeat = repeat_on_fail
         try:
@@ -18,6 +20,7 @@ class FaceBookMessageBot:
             print(str(ex))
             if repeat > 0:
                 sleep(30)
+                self.login(self.email, self.passwd)
                 self.send_message(message, thread_id, thread_type, repeat - 1)
             else:
                 raise ex
@@ -25,7 +28,6 @@ class FaceBookMessageBot:
     def send_message_my(self, message):
         thread_type = ThreadType.USER
         self.client.sendMessage(message, thread_id=self.client.client_id, thread_type=thread_type)
-
 
     def get_messages(self, thread_id, limit=30, before=None):
         return self.client.fetchThreadMessages(thread_id, limit, before)
