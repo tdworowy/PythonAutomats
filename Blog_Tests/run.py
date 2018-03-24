c6fcbc12da7f5f74387d93be3d5fefbc0479c8fc
Tomasz <dworowytomasz@gmail.com>
2018-03-24 11:25:19 +0100

    fix

--- Blog_Tests/run.py
+++ Blog_Tests/run.py
@@ -6,8 +6,9 @@ from Blog_Tests.screens.screenPath import get_screen_path
 def run_behave():
     path = os.path.dirname(os.path.abspath(__file__))
     screen_path = get_screen_path()
-    partition = "E:"
-    commend ='%s && cd "%s" && behave -f allure_behave.formatter:AllureFormatter -o "%s" ./features' %(partition,path,screen_path)
+    partition = "D:"
+    commend = '%s && cd "%s" && behave -f allure_behave.formatter:AllureFormatter -o "%s" ./features' % (
+        partition, path, screen_path)
     print(commend)
     os.system(commend)
 
