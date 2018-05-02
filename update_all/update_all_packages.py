753d87450b8458a47e10c23a9da8bcd1c5bf6e86
Tomasz <dworowytomasz@gmail.com>
2018-05-02 22:50:00 +0200

    fix

--- update_all/update_all_packages.py
+++ update_all/update_all_packages.py
@@ -1,10 +1,10 @@
 from subprocess import call
 
-import pip
+from pip import _internal
 
 
 def update_all():
-    packages = [dist.project_name for dist in pip.get_installed_distributions()]
+    packages = [dist.project_name for dist in _internal.get_installed_distributions()]
     for package in packages:
         try:
             call("pip install --upgrade " + str(package), shell=True)
