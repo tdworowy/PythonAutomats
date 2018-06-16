dcd643dc8811a32ad406931dffe091f54fba09e3
Tomasz <dworowytomasz@gmail.com>
2018-06-16 16:50:34 +0200

    added roobal test

---
+++ PythonAutomats/Roobal/request_test.py
@@ -0,0 +1,14 @@
+import requests
+
+
+def test_roobal():
+    """Test roobal page"""
+    response = requests.get("http://robertmarek.pl")
+    while 1:
+        print(response)
+        if "500" in response:
+            break
+
+
+if __name__ == "__main__":
+    test_roobal()
