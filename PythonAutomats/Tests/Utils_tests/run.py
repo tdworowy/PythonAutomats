a6d70cd3762a081623ac411e26de3bcabef1b698
Tomasz <dworowytomasz@gmail.com>
2018-07-09 14:38:01 +0200

    add run function

---
+++ PythonAutomats/Tests/Utils_tests/run.py
@@ -0,0 +1,12 @@
+import os
+
+
+def run():
+    path = os.path.dirname(os.path.abspath(__file__))
+    files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+    files = [f for f in files if "_test" in f]
+    list(map(lambda file: os.system(r"python -m pytest --junit-xml=pytest_unit.xml %s" % file), files))
+
+
+if __name__ == "__main__":
+    run()
