cf585ea60f915912f312dbb8ad549177fdc7f77a
Tomasz <dworowytomasz@gmail.com>
2019-10-04 15:39:57 +0200

    add first aorflow exampel

---
+++ PythonAutomats/Airflow/get_songs_from_last_fm.py
@@ -0,0 +1,35 @@
+
+from __future__ import print_function
+
+import time
+from builtins import range
+from pprint import pprint
+
+import airflow
+from Songs.last_fm_parser import get_titles
+from airflow.models import DAG
+from airflow.operators.python_operator import PythonOperator
+
+args = {
+    'owner': 'airflow',
+}
+
+dag = DAG(
+    dag_id='get_songs_from_last_fm',
+    default_args=args,
+    schedule_interval=None,
+)
+
+def get_songs(ds, **kwargs):
+    get_titles('https://www.last.fm/pl/user/TotaledThomas/library/tracks')
+    get_titles('https://www.last.fm/pl/user/TheRoobal/library/tracks')
+
+
+run_this = PythonOperator(
+    task_id='get_songs_from_last_fm',
+    provide_context=True,
+    python_callable=get_songs,
+    dag=dag,
+)
+
+run_this
