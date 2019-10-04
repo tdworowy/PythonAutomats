9332cabccee73bdaaa5c701420294a600ffb867e
Tomasz <dworowytomasz@gmail.com>
2019-10-04 16:46:33 +0200

    add print

---
+++ PythonAutomats/Airflow/MY_print.py
@@ -0,0 +1,34 @@
+
+from __future__ import print_function
+
+import time
+from builtins import range
+from pprint import pprint
+
+import airflow
+from airflow.models import DAG
+from airflow.operators.python_operator import PythonOperator
+
+args = {
+    'owner': 'airflow',
+    'start_date': airflow.utils.dates.days_ago(2),
+}
+
+dag = DAG(
+    dag_id='get_songs_from_last_fm',
+    default_args=args,
+    schedule_interval=None,
+)
+
+def my_print(ds, **kwargs):
+   print("TEST")
+
+
+run_this = PythonOperator(
+    task_id='get_songs_from_last_fm',
+    provide_context=True,
+    python_callable=my_print,
+    dag=dag,
+)
+
+run_this
--- PythonAutomats/Airflow/prepare.sh
+++ PythonAutomats/Airflow/prepare.sh
@@ -1,1 +1,2 @@
 cp get_songs_from_last_fm.py /home/tom/airflow/dags/
+cp My_print.py /home/tom/airflow/dags/
