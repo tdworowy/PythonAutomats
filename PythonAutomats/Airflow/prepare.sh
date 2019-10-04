b179894446ed234f0fb81da9c4d1541c53369d90
Tomasz <dworowytomasz@gmail.com>
2019-10-04 15:57:27 +0200

    update

--- PythonAutomats/Airflow/prepare.sh
+++ PythonAutomats/Airflow/prepare.sh
@@ -1,3 +1,3 @@
-export PYTHONPATH="" > ~/.bashrc
-export PYTHONPATH=${PYTHONPATH}:..\${PWD} >> ~/.bashrc
+echo "export PYTHONPATH=" > ~/.bashrc
+echo "export PYTHONPATH=${PYTHONPATH}:..\${PWD}" >> ~/.bashrc
 cp get_songs_from_last_fm.py /home/tom/airflow/dags/
